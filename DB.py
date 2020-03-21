f5ec412936ecac59e3a037c30663b7121c96ddda
Roman-Borysenko <borysenko.roman18@gmail.com>
2020-03-22 00:30:32 +0200

    added a method to add records to the database

---
+++ DB.py
@@ -0,0 +1,10 @@
+import json
+import pymysql
+from pymysql.cursors import DictCursor
+
+def insert(field, date, ndvi, other_data, path, cloudiness, avg, std, sampling_rate):
+    connection = pymysql.connect(host='127.0.0.1', user='root', password='12345', db='python', charset='utf8mb4', cursorclass=DictCursor)
+    with connection.cursor() as cursor:
+        query = "INSERT INTO `data_image` (`field`, `date`, `ndvi`, `other_data`, `path`, `cloudiness`, `average`, `std`, `json`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        cursor.execute(query, (field, date, ndvi, other_data, str(path), float(cloudiness), float(avg), float(std), json.dumps(str(sampling_rate))))
+        connection.commit()
--- Main.py
+++ Main.py
@@ -1,18 +1,24 @@
-from collections import Counter
+from datetime import datetime
 
+import pymysql
+from pymysql.cursors import DictCursor
 import rasterio
 import os
 import numpy as np
+from collections import Counter
+import DB
+
+
+# connection = pymysql.connect(host='127.0.0.1', user='root', password='12345', db='python', charset='utf8mb4', cursorclass=DictCursor)
 
 
 def listMatrixImage(path):
-    images = []
     files = os.listdir(path)
     for file in files:
-        print(file.split("_"))
+        element = file.split("_")
         with rasterio.open(path + file, 'r') as ds:
-            images.append(ds.read()[0])
-    return images
+            arr = ds.read()[0]
+            DB.insert(element[1], datetime.strptime(element[0], '%d%m%Y').date(), element[3], element[4], path+file, cloudiness(arr), np.average(arr), np.std(arr), dict(Counter(arr[1].ravel())))
 
 
 def outputMatrix(array):
@@ -31,7 +37,9 @@ def cloudiness(array):
     return count / np.size(array)
 
 
-arr = listMatrixImage("D:/python/test/")
+listMatrixImage("D:/python/test/")
 # print(np.average(arr[1])) # середнє зважене
 # print(np.std(arr[1])) # середнє квадратичне відхилення
-#print(Counter(arr[1].ravel()))  # частот входження унікальних значень у вибірку
+# print(dict(Counter(arr[1].ravel())))  # частот входження унікальних значень у вибірку
+
+# connection.close()
