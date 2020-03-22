3e67b4ff7189500bd24bffc919a8bc019323a5de
Roman-Borysenko <borysenko.roman18@gmail.com>
2020-03-22 16:13:11 +0200

    added data output for the field

--- DB.py
+++ DB.py
@@ -8,3 +8,11 @@ def insert(field, date, ndvi, other_data, path, cloudiness, avg, std, sampling_r
         query = "INSERT INTO `data_image` (`field`, `date`, `ndvi`, `other_data`, `path`, `cloudiness`, `average`, `std`, `json`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
         cursor.execute(query, (field, date, ndvi, other_data, str(path), float(cloudiness), float(avg), float(std), json.dumps(str(sampling_rate))))
         connection.commit()
+
+
+def select(field):
+    connection = pymysql.connect(host='127.0.0.1', user='root', password='12345', db='python', charset='utf8mb4', cursorclass=DictCursor)
+    with connection.cursor() as cursor:
+        query = "SELECT * FROM python.data_image where `field` = %s Order By `date`"
+        cursor.execute(query, field)
+        return cursor.fetchall()
--- Main.py
+++ Main.py
@@ -6,8 +6,8 @@ import numpy as np
 from collections import Counter
 import DB
 
-
 globalCounter = 0
+
 
 def listMatrixImage(path):
     count = 0;
@@ -16,7 +16,8 @@ def listMatrixImage(path):
         element = file.split("_")
         with rasterio.open(path + file, 'r') as ds:
             arr = ds.read()[0]
-            DB.insert(element[1], datetime.strptime(element[0], '%d%m%Y').date(), element[3], element[4], path+file, cloudiness(arr), np.average(arr), np.std(arr), dict(Counter(arr[1].ravel())))
+            DB.insert(element[1], datetime.strptime(element[0], '%d%m%Y').date(), element[3], element[4], path + file,
+                      cloudiness(arr), np.average(arr), np.std(arr), dict(Counter(arr[1].ravel())))
             count += 1;
             print(count)
 
@@ -27,10 +28,9 @@ def taskListMatrixImage(path, taskId, begin, end):
         element = files[i].split("_")
         with rasterio.open(path + files[i], 'r') as ds:
             arr = ds.read()[0]
-            DB.insert(element[1], datetime.strptime(element[0], '%d%m%Y').date(), element[3], element[4], path+files[i], cloudiness(arr), np.average(arr), np.std(arr), dict(Counter(arr[1].ravel())))
+            DB.insert(element[1], datetime.strptime(element[0], '%d%m%Y').date(), element[3], element[4],
+                      path + files[i], cloudiness(arr), np.average(arr), np.std(arr), dict(Counter(arr[1].ravel())))
             print(taskId)
-
-
 
 
 def outputMatrix(array):
@@ -60,5 +60,15 @@ def createTask(threadsCount, records):
         end = begin + records
 
 
-#createTask(3, 10) # test
-createTask(5, 18181)
+def outputFieldData():
+    field = input("Введіть назву поля: ")
+    result = DB.select(field)
+    print("%5s\t|\t%10s\t|\t%10s\t|\t%5s\t|\t%5s\t|\t%5s" % ("ID", "Date", "Field", "Cloudiness", "Average", "Srd"))
+    for item in result:
+        print("%5d\t|\t%10s\t|\t%10s\t|\t%5s\t|\t%5s\t|\t%5s" % (item["id"], item["date"], item["field"], round(item["cloudiness"], 4), round(item["average"], 2), round(item["std"], 2)))
+
+
+# createTask(3, 10) # test
+#createTask(5, 18181)
+
+outputFieldData()
